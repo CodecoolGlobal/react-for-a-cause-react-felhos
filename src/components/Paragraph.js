a9e61a793506c9608d2194520c2daaecf4b51ac0
ghxst0 <lfoxy18@gmail.com>
2023-01-10 14:57:46 +0100

    add donate section

--- src/components/Paragraph.js
+++ src/components/Paragraph.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 export default function Paragraph({title, text}) {
   return (
-    <>
+    <div>
         <h3>{title}</h3>
         <p>{text}</p>
-    </>
+    </div>
   )
 }
