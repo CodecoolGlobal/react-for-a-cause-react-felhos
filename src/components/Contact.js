fff0f80ea87baec6a8cce4d4288728834453f98e
ghxst0 <lfoxy18@gmail.com>
2023-01-10 14:02:19 +0100

    add contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,20 @@
-function Contact(props) {
+import "./css/ContactForm.css"
 
+function Contact() {
+    return (
+        <div className="form-wrapper">
+            <form className="ContactForm">
+                <div class="name-input">
+                    <input type="text" placeholder="first name"/>
+                    <input type="text" placeholder="last name"/>
+                </div>
+                <input type="email" placeholder="example@mail.com"/>
+                <input type="text" placeholder="subject"/>
+                <textarea name="Text1" cols="40" rows="5"></textarea>
+                <input type="submit" value="Submit"/>
+            </form>
+        </div>
+    )
 }
   
 export default Contact;
