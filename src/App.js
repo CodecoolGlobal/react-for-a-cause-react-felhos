0f449739096a838e33d4fa12bd2a2c3db0f94bde
Le Marcell <73469368+felhos@users.noreply.github.com>
2023-01-10 15:04:40 +0100

    made it temporarily uglier

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Header from'./components/Header'
 import About from'./components/About'
 import Welcome from'./components/Welcome'
 import Contact from'./components/Contact'
+import Hero from'./components/Hero'
+import Donate from'./components/Donate'
 import './App.css';
 
 function App() {
@@ -18,9 +20,11 @@ function App() {
   return (
     <div className="App">
       {<Header logo={logo} func={setContent}/>}
+      {<Hero/>}
       <div class="container">
         {contentOptions[content]}
       </div>
+      {<Donate/>}
     </div>
   );
 }
