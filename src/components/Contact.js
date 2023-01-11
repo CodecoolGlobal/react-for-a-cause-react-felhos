7299affd2f9d3672f7255469feea6a60749272d9
Le Marcell <73469368+felhos@users.noreply.github.com>
2023-01-11 13:48:02 +0100

    send form makes the object and logs it

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,17 +1,27 @@
 import "./css/ContactForm.css"
 
 function Contact() {
+    function sendForm() {
+        let formObject = {};
+        formObject.firstName = document.querySelector("#firstName").value;
+        formObject.lastName = document.querySelector("#lastName").value;    
+        formObject.email = document.querySelector("#email").value;  
+        formObject.subject = document.querySelector("#subject").value; 
+        formObject.messageText = document.querySelector("#messageText").value; 
+        console.log(formObject);
+    }
+
     return (
         <div className="form-wrapper">
-            <form className="ContactForm">
+            <form className="ContactForm" action="javascript:void(0);">
                 <div class="name-input">
-                    <input type="text" placeholder="first name"/>
-                    <input type="text" placeholder="last name"/>
+                    <input id= "firstName" type="text" placeholder="first name"/>
+                    <input id= "lastName" type="text" placeholder="last name"/>
                 </div>
-                <input type="email" placeholder="example@mail.com"/>
-                <input type="text" placeholder="subject"/>
-                <textarea name="Text1" cols="40" rows="5"></textarea>
-                <input type="submit" value="Submit"/>
+                <input id="email" type="email" placeholder="example@mail.com"/>
+                <input id="subject" type="text" placeholder="subject"/>
+                <textarea id="messageText" name="Text1" cols="40" rows="5"></textarea>
+                <button onClick={sendForm}>Send</button>
             </form>
         </div>
     )
